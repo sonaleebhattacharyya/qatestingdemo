d169d12e205a1ec4c705308ff53f8999ab3387e1
Sonalee Bhattacharyya <sonaleebhattacharyya@gmail.com>
2022-12-12 14:56:30 -0600

    qatestingdemo

--- index.test.js
+++ index.test.js
@@ -15,3 +15,55 @@ let testData = {
     floor: 5,
     roomNumber: 3
   }
+
+  test('test function',() => {
+    expect(2).toBe(2)
+  })
+
+  test('first name should be patten',()=> {
+    expect(testData.firstName).toBe('Patten')
+  })
+
+  //describe block to group tests for formatTitle function
+  describe('formatTitle tests', () => {
+
+    test('formatTitle should return a string',() => {
+      let formattedTitle = formatTitle(testData.title)
+      expect(typeof formattedTitle).toBe('string')
+    })
+
+    test('formatTitle should format title correctly',() => {
+      let formattedTitle = formatTitle(testData.title)
+      expect(formattedTitle).toBe('Nulla Ac');
+    })
+
+  }) 
+
+  describe('shortenBio tests',() => {
+    let shortBio = shortenBio(testData.bio)
+    test('shortenBio will return a smaller Bio string',() => {
+      let shortBio = shortenBio(testData.bio)
+      expect(shortBio.length).toBeLessThan(testData.bio.length)
+    })
+    test('shortenBio should add ... to the end of the line', () => expect(shortBio).toContain('...'))
+    })
+    describe('convertLength tests',()=> {
+    let result = convertLength(testData.length)
+    let resultTwo = convertLength(30)
+
+
+    test('convertLength should return an array with two elements',() => expect(result).toHaveLength(2))
+    test('convertLength can handle numbers less than 60',() => expect(resultTwo[1]).toEqual(30))
+    })
+
+    test('toBe vs toEqual',() => {
+      const myObj = {myNum:4}
+      const myObjTwo = {myNum:4}
+      expect(myObj).toBe(myObjTwo)
+    })
+
+    test('toEqual',() => {
+      const myObj = {myNum:4}
+      const myObjTwo = {myNum:4}
+      expect(myObj).toEqual(myObjTwo)
+    })
